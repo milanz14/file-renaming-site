4db4d7f7f234e80ddb166b25cb07b10df9e0cc91
Milan Zagorac <zagoracm@gmail.com>
2023-06-20 10:14:37 -0400

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const handleDropEvent = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log("file dropped...");
+  };
+
+  const handleDragOver = () => {
+    console.log("file dragged over...");
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        Drop files <i>below</i>
       </p>
+      <div
+        className="drop-zone"
+        onDrop={handleDropEvent}
+        onDragOver={handleDragOver}></div>
     </div>
   );
 }
