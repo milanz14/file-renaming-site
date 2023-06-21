6af2982ee8c201b4123702db9d683c27bbcabb46
Milan Zagorac <zagoracm@gmail.com>
2023-06-21 15:12:16 -0400

    added click to open file browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handleDropEvent = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     console.log("file dropped...");
@@ -10,14 +13,24 @@ function App() {
     console.log("file dragged over...");
   };
 
+  const handleClick = () => {
+    // add type guard to prevent typescript errors
+    if (inputRef.current !== null) {
+      inputRef.current.click();
+    }
+  };
+
   return (
     <div
       className="drop-zone"
       onDrop={handleDropEvent}
-      onDragOver={handleDragOver}>
+      onDragOver={handleDragOver}
+      onClick={handleClick}>
       <span className="drop-zone__prompt">
         Drop items here or <b>click</b> to add...
       </span>
+
+      <input className="file-browser" type="file" ref={inputRef} />
     </div>
   );
 }
